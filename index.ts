08450244a095508a77c90635bcd1f34e079f2260
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2025-01-28 19:14:44 +0100

    Reverted index to original

--- index.ts
+++ index.ts
@@ -1,20 +1,3 @@
-/**
- * Function that greets a person
- * @param firstname First name
- * @param lastname Last name
- */
-
 export const greeting = (firstname: string, lastname: string) => {
-    console.log(`Hei hei ${firstname} ${lastname}`);
-  };
-  
-  const people = [
-    { firstname: "Magnus", lastname: "Magnusson" },
-    { firstname: "Stefan", lastname: "Stefanson" },
-  ];
-  
-  export const greetTheGang = () => {
-    people.forEach((person) => {
-      greeting(person.firstname, person.lastname);
-    });
-  };
+  console.log(`Hello ${firstname} ${lastname}`);
+}
